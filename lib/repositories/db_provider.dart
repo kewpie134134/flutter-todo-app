8d3bc3ab225885091d610113e0e8c0e054a89cd8
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-08-31 15:01:27 +0900

    CRUD関数を作成

--- lib/repositories/db_provider.dart
+++ lib/repositories/db_provider.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter_todo_app/models/todo.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 
@@ -10,6 +12,8 @@ class DBProvider {
   static final DBProvider db = DBProvider._();
 
   static Database _database;
+  // sqliteで使用するテーブルネームをセットする
+  static final _tableName = "Todo";
 
   Future<Database> get database async {
     if (_database != null) return _database;
@@ -33,5 +37,33 @@ class DBProvider {
         "dueDate TEXT,"
         "note TEXT"
         ")");
+  }
+
+  // 以下、CRUD関数
+  createTodo(Todo todo) async {
+    final db = await database;
+    var res = await db.insert(_tableName, todo.toMap());
+    return res;
+  }
+
+  getAllTodos() async {
+    final db = await database;
+    var res = await db.query(_tableName);
+    List<Todo> list =
+        res.isNotEmpty ? res.map((c) => Todo.fromMap(c)).toList() : [];
+    return list;
+  }
+
+  updateTodo(Todo todo) async {
+    final db = await database;
+    var res = await db.update(_tableName, todo.toMap(),
+        where: "id = ?", whereArgs: [todo.id]);
+    return res;
+  }
+
+  deleteTodo(String id) async {
+    final db = await database;
+    var res = db.delete(_tableName, where: "id = ?", whereArgs: [id]);
+    return res;
   }
 }
